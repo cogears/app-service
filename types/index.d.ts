742aa5362c79d432201115f12c072ea076f718b4
stone <guolingen@qmxdata.com>
2023-10-23 13:48:59 +0800

    app.dispose

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,4 +8,5 @@ export default class AppContext {
     installStorage(config: MysqlConfig): Promise<any>;
     schedule(task: Task, delay?: number): TaskHandle;
     schedulePeriodTask(task: Task, period: number): TaskHandle;
+    dispose(): void;
 }
