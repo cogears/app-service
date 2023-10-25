7ab17dca3a3a586f860bdc6953d341d9bdb836a1
stone <guolingen@qmxdata.com>
2023-10-25 16:32:12 +0800

    fix bug

--- src/core/InternalContext.ts
+++ src/core/InternalContext.ts
@@ -49,8 +49,8 @@ export default class InternalContext {
     }
 
     dispose() {
-        if (this.storage) {
-            this.storage.dispose();
+        if (this._storage) {
+            this._storage.dispose();
         }
         this._taskManager.dispose();
     }
